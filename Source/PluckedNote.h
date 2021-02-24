4371f2bbc548bd83c31d38a0f13c65ff3f276449
mhamilt <matt.hamilton@ed.ac.uk>
2021-02-24 12:17:19 +0000

    📒add documentation

--- Source/PluckedNote.h
+++ Source/PluckedNote.h
@@ -1,11 +1,11 @@
 /*
-   ==============================================================================
-
-   PluckedNote.h
-   Created: 23 Feb 2021 6:04:44pm
-   Author:  csmit
-
-   ==============================================================================
+ ==============================================================================
+ 
+ PluckedNote.h
+ Created: 23 Feb 2021 6:04:44pm
+ Author:  csmit
+ 
+ ==============================================================================
  */
 
 #pragma once
@@ -13,35 +13,44 @@
 #include <tgmath.h>
 #include <JuceHeader.h>
 
+/// <#Description#>
 class PluckedNote
 {
-public:
-  PluckedNote();
-  ~PluckedNote();
-
-  //=============================================================================
-  void generateNote();
-  float process();
-  //=============================================================================
-  #pragma mark getters and setters
-
-  void setFrequency(float freq);
-  void setSampleRate(float SR);
-  void setNoteLength(float noteLength);
-
+public:    
+    PluckedNote();
+    ~PluckedNote();
+    
+    //=============================================================================
+    /// <#Description#>
+    void generateNote();
+    /// <#Description#>
+    float process();
+    //=============================================================================
+#pragma mark getters and setters
+    
+    /// <#Description#>
+    /// @param freq <#freq description#>
+    void setFrequency(float freq);
+    /// <#Description#>
+    /// @param SR <#SR description#>
+    void setSampleRate(float SR);
+    /// <#Description#>
+    /// @param noteLength <#noteLength description#>
+    void setNoteLength(float noteLength);
+    
 private:
-  /// frequency of plucked note variable
-  float frequency = 440.0f;
-  /// stored sample rate
-  float sampleRate = 48000.0f;
-  /// dynamic parameter
-  float dynParam = 0.95f;
-  /// length of note in seconds
-  float T60 = 2.0f;
-  /// initialize phase
-  float phase = 0.0f;
-  /// storing note data
-  juce::Array<float> waveTable;
-  /// length in samples
-  float wtSize = floor(sampleRate * T60);
+    /// frequency of plucked note variable
+    float frequency = 440.0f;
+    /// stored sample rate
+    float sampleRate = 48000.0f;
+    /// dynamic parameter
+    float dynParam = 0.95f;
+    /// length of note in seconds
+    float T60 = 2.0f;
+    /// initialize phase
+    float phase = 0.0f;
+    /// storing note data
+    juce::Array<float> waveTable;
+    /// length in samples
+    float wtSize = floor(sampleRate * T60);
 };
